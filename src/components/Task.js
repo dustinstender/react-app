e481b061b66f8530b73211226cc6ca9de785ddf0
Dustin Stender <dustinpstender@gmail.com>
2021-10-25 12:35:45 +0200

    toggle complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task" onDoubleClick={() => onToggle(task.id)}>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
       <h3>
         {task.name}{" "}
         <FaTimes
